29ee43f5650355992a67898bfea9e0716c054ede
yazdes <83878758+yatolya@users.noreply.github.com>
2023-06-29 21:32:37 +0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,358 @@
+﻿namespace VkSearchER
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FirstName = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.labellastseen = new System.Windows.Forms.Label();
+            this.CountSerchTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.BirthDayTextBox = new System.Windows.Forms.TextBox();
+            this.BirthMothTextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.AgeFromEditB = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.AgeToEditB = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // FirstName
+            // 
+            this.FirstName.AutoSize = true;
+            this.FirstName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Bold);
+            this.FirstName.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.FirstName.Location = new System.Drawing.Point(12, 9);
+            this.FirstName.Name = "FirstName";
+            this.FirstName.Size = new System.Drawing.Size(176, 16);
+            this.FirstName.TabIndex = 1;
+            this.FirstName.Text = "Павел Павелочинский";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.AliceBlue;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button2.Location = new System.Drawing.Point(85, 125);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(342, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Поиск";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.BackColor = System.Drawing.Color.AliceBlue;
+            this.listBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.75F);
+            this.listBox2.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 15;
+            this.listBox2.Location = new System.Drawing.Point(85, 154);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(342, 287);
+            this.listBox2.TabIndex = 10;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            this.listBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.listBox2_KeyDown);
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(433, 125);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Обновить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button4
+            // 
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button4.Location = new System.Drawing.Point(433, 183);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(138, 23);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Записать в файл";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // labellastseen
+            // 
+            this.labellastseen.AutoSize = true;
+            this.labellastseen.BackColor = System.Drawing.Color.Transparent;
+            this.labellastseen.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labellastseen.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.labellastseen.Location = new System.Drawing.Point(12, 26);
+            this.labellastseen.Name = "labellastseen";
+            this.labellastseen.Size = new System.Drawing.Size(81, 13);
+            this.labellastseen.TabIndex = 13;
+            this.labellastseen.Text = "Был в сети: ";
+            // 
+            // CountSerchTextBox
+            // 
+            this.CountSerchTextBox.BackColor = System.Drawing.Color.AliceBlue;
+            this.CountSerchTextBox.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.CountSerchTextBox.Location = new System.Drawing.Point(95, 94);
+            this.CountSerchTextBox.Name = "CountSerchTextBox";
+            this.CountSerchTextBox.Size = new System.Drawing.Size(61, 20);
+            this.CountSerchTextBox.TabIndex = 14;
+            this.CountSerchTextBox.Text = "20";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(51, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Кол-в:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(162, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "День рож.:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(270, 97);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Месяц рож.:";
+            // 
+            // BirthDayTextBox
+            // 
+            this.BirthDayTextBox.BackColor = System.Drawing.Color.AliceBlue;
+            this.BirthDayTextBox.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.BirthDayTextBox.Location = new System.Drawing.Point(227, 90);
+            this.BirthDayTextBox.Name = "BirthDayTextBox";
+            this.BirthDayTextBox.Size = new System.Drawing.Size(36, 20);
+            this.BirthDayTextBox.TabIndex = 18;
+            // 
+            // BirthMothTextBox
+            // 
+            this.BirthMothTextBox.BackColor = System.Drawing.Color.AliceBlue;
+            this.BirthMothTextBox.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.BirthMothTextBox.Location = new System.Drawing.Point(339, 94);
+            this.BirthMothTextBox.Name = "BirthMothTextBox";
+            this.BirthMothTextBox.Size = new System.Drawing.Size(36, 20);
+            this.BirthMothTextBox.TabIndex = 19;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.label6.Location = new System.Drawing.Point(12, 39);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(78, 13);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "Сообщения:";
+            // 
+            // button5
+            // 
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button5.Location = new System.Drawing.Point(433, 154);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 21;
+            this.button5.Text = "В буфер";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(162, 68);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(68, 13);
+            this.label7.TabIndex = 22;
+            this.label7.Text = "Возраст: От";
+            // 
+            // AgeFromEditB
+            // 
+            this.AgeFromEditB.BackColor = System.Drawing.Color.AliceBlue;
+            this.AgeFromEditB.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.AgeFromEditB.Location = new System.Drawing.Point(227, 64);
+            this.AgeFromEditB.Name = "AgeFromEditB";
+            this.AgeFromEditB.Size = new System.Drawing.Size(36, 20);
+            this.AgeFromEditB.TabIndex = 23;
+            this.AgeFromEditB.Text = "18";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(314, 68);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(25, 13);
+            this.label8.TabIndex = 24;
+            this.label8.Text = "До:";
+            // 
+            // AgeToEditB
+            // 
+            this.AgeToEditB.BackColor = System.Drawing.Color.AliceBlue;
+            this.AgeToEditB.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.AgeToEditB.Location = new System.Drawing.Point(339, 64);
+            this.AgeToEditB.Name = "AgeToEditB";
+            this.AgeToEditB.Size = new System.Drawing.Size(36, 20);
+            this.AgeToEditB.TabIndex = 25;
+            this.AgeToEditB.Text = "0";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.label9.Location = new System.Drawing.Point(11, 52);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(64, 13);
+            this.label9.TabIndex = 26;
+            this.label9.Text = "Возраст: ";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.checkBox2.Location = new System.Drawing.Point(381, 97);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(87, 17);
+            this.checkBox2.TabIndex = 28;
+            this.checkBox2.Text = "Отсев по ЛС";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(422, 443);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(165, 13);
+            this.linkLabel1.TabIndex = 29;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "github.com/yatolya/VKSearchER";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "vk.com/id",
+            "https://vk.com/id",
+            "id",
+            "Свой"});
+            this.comboBox1.Location = new System.Drawing.Point(435, 212);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 30;
+            this.comboBox1.Text = "Формат записи id";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.comboBox1.TextUpdate += new System.EventHandler(this.comboBox1_TextUpdate);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.AliceBlue;
+            this.ClientSize = new System.Drawing.Size(585, 457);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.AgeToEditB);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.AgeFromEditB);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.BirthMothTextBox);
+            this.Controls.Add(this.BirthDayTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.CountSerchTextBox);
+            this.Controls.Add(this.labellastseen);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.FirstName);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "VkSearchER";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label FirstName;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label labellastseen;
+        private System.Windows.Forms.TextBox CountSerchTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox BirthDayTextBox;
+        private System.Windows.Forms.TextBox BirthMothTextBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox AgeFromEditB;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox AgeToEditB;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using VkNet;
+using VkNet.Enums.Filters;
+using VkNet.Model;
+using VkNet.Model.RequestParams;
+using System.Diagnostics;
+using System.Text;
+using System.Configuration;
+
+namespace VkSearchER
+{
+
+    public partial class Form1 : Form
+    {
+        public string AToken = ConfigurationManager.AppSettings["access_token"];
+        public VkApi api = new VkApi();
+        public long UId = 1;
+        public uint CountSercher = 20;
+        public ushort BirthDaySet = 01;
+        public ushort BirthMothSet = 01;
+        public ushort AgeFromSet = 18;
+        public ushort AgeToSet = 0;
+        public string StrSiteId = "";
+
+        public void Auths()
+        {
+            try
+            {
+                if (AToken != null)
+                {
+                    api.Authorize(new ApiAuthParams
+                    {
+                        AccessToken = AToken,
+                        Settings = Settings.All
+                    });
+                    GetInfo(UId);
+                }
+                else
+                {
+                    MessageBox.Show("Ошибка Авторизации: Не найден ключ 'access_token' проверьте конфиг-файл 'VkSearchER.exe.config' !");
+                    Process.GetCurrentProcess().Kill();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка Авторизации: " + ex.Message);
+                Process.GetCurrentProcess().Kill();
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        public void GetInfo(long uids)
+        {
+            var ppf = ProfileFields.LastSeen | ProfileFields.CanWritePrivateMessage | ProfileFields.BirthDate | ProfileFields.Online | ProfileFields.City;
+            var p = api.Users.Get(new long[] { uids }, ppf).FirstOrDefault();
+            if (p == null)
+            {
+                FirstName.Text = "Нет данных";
+                return;
+            }
+
+            var onlineprofile = (bool)p.Online ? "Online" : "Offline";
+            var cuty = p.City?.Title ?? "Скрыт";
+            var ls = p.CanWritePrivateMessage ? "Сообщения: Открыты" : "Сообщения: Закрыты";
+            var lastSeen = p.LastSeen == null ? "Был в сети: Давно" : $"Был в сети: {TimeZoneInfo.ConvertTimeFromUtc(DateTime.Parse(p.LastSeen.Time.ToString()), TimeZoneInfo.FindSystemTimeZoneById("Arabian Standard Time"))}";
+            var birthDate = DateTime.Parse(p.BirthDate);
+            var age = (int)((DateTime.Today - birthDate).TotalDays / 365);
+            labellastseen.Text = lastSeen;
+            label9.Text = $"Возраст: {p.BirthDate} ({age})";
+            label6.Text = ls;
+            FirstName.Text = $"{p.FirstName} {p.LastName} ({onlineprofile}) Г.{cuty}";
+        }
+
+        public void ReadEBox()
+        {
+            BirthDaySet = Convert.ToUInt16(BirthDayTextBox.Text);
+            BirthMothSet = Convert.ToUInt16(BirthMothTextBox.Text);
+            CountSercher = Convert.ToUInt32(CountSerchTextBox.Text);
+            AgeFromSet = Convert.ToUInt16(AgeFromEditB.Text);
+            AgeToSet = Convert.ToUInt16(AgeToEditB.Text);
+        }
+        public void SearchFriends(bool checkCanWritePrivateMessage)
+        {
+            ReadEBox();
+            Random rans = new Random();
+            uint uintValue = (uint)rans.Next(0, 100);
+            VkNet.Utils.VkCollection<User> PeopleSearch = api.Users.Search(new UserSearchParams()
+             {
+                 Fields = ProfileFields.CanWritePrivateMessage,
+                 Count = CountSercher,
+                 Offset = uintValue,
+                 AgeFrom = AgeFromSet,
+                 AgeTo = AgeToSet,
+                 BirthDay = BirthDaySet,
+                 BirthMonth = BirthMothSet,
+                 Online = true
+             });
+             
+            while (!PeopleSearch.Any())
+            {
+                PeopleSearch = api.Users.Search(new UserSearchParams()
+                {
+                    Fields = ProfileFields.CanWritePrivateMessage,
+                    Count = CountSercher,
+                    Offset = uintValue,
+                    AgeFrom = AgeFromSet,
+                    AgeTo = AgeToSet,
+                    BirthDay = BirthDaySet,
+                    BirthMonth = BirthMothSet,
+                    Online = true
+                });
+            }
+            foreach (var resultsearch in PeopleSearch)
+            {
+                 if (!checkCanWritePrivateMessage || resultsearch.CanWritePrivateMessage)
+                 {
+                     listBox2.Items.Add(resultsearch.Id.ToString());
+                 }
+                button2.Text = $"Поиск ({listBox2.Items.Count.ToString()})";
+            }
+        }
+       
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ReadEBox();
+            listBox2.Items.Clear();
+            if (CountSercher > 1000)
+            {
+                MessageBox.Show("Не больше 1к, ограничение VkNET");
+            }
+            else
+            {
+                AgeFromSet = (AgeFromSet < 18) ? (ushort)18 : AgeFromSet;
+                AgeToSet = (AgeToSet > 40) ? (ushort)0 : AgeToSet;
+                SearchFriends(checkBox2.Checked);
+            }
+
+        }
+        public void SetDate()
+        {
+            DateTime now = DateTime.Now;
+            int day = now.Day;
+            int month = now.Month;
+            BirthDayTextBox.Text = day.ToString();
+            BirthMothTextBox.Text = month.ToString();
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SetDate();
+            Auths();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            SearchFriends(checkBox2.Checked);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string path = "Result";
+            string filedataname = $"{DateTime.Now.ToShortDateString()}({listBox2.Items.Count})";
+
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            if (listBox2.Items.Count > 0)
+            {
+                foreach (string item in listBox2.Items)
+                {
+                    using (StreamWriter writer = new StreamWriter($"{path}/{filedataname}.txt", true))
+                    {
+                        writer.WriteLineAsync(StrSiteId + item.ToString());
+                    }
+                }
+            }
+           
+        }
+
+        private void listBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.C)
+            {
+                if (listBox2.SelectedItem != null)
+                {
+                    Clipboard.SetText($"vk.com/id{listBox2.SelectedItem.ToString()}");
+                }
+            }
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox2.SelectedItem != null)
+            {
+                GetInfo(long.Parse(listBox2.SelectedItem.ToString()));
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (listBox2.Items.Count > 0)
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach (var item in listBox2.Items)
+                {
+                    sb.AppendLine(StrSiteId + item.ToString());
+                }
+                Clipboard.SetText(sb.ToString());
+            }
+        
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://github.com/yatolya/VKSearchER");
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            StrSiteId = comboBox1.Text;
+        }
+
+        private void comboBox1_TextUpdate(object sender, EventArgs e)
+        {
+            StrSiteId = comboBox1.Text;
+        }
+    }
+}
